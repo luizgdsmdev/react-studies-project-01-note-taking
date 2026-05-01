5ee7757e036c202fc990c853a14aa2f14acaf1d9
Luiz Messias <luizgdsmdev@gmail.com>
2026-05-01 00:50:22 -0300

    fix(app): remove unwanted packege.json and adjust trivial styling for App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,7 +9,7 @@ import { Routes, Route } from "react-router-dom";
  */
 function App() {
   return (
-    <div data-theme="retro" className="relative min-h-full w-full">
+    <div data-theme="dark" className="relative min-h-full w-full">
       <div className="fixed inset-0 -z-10 h-full w-full bg-base-300" />
       <div className="relative z-0 min-h-full w-full">
         <Routes>
